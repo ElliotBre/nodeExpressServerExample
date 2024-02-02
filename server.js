b5dc9671005e2dd990ff7b9d1b5856259a55ec64
Elliot Brooks <elliot.brooks@sky.uk>
2024-02-02 13:57:53 +0000

    added working setup for delivering html index page

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 
 import express from 'express';
 import path from 'path'
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, 'html/index.html'));
 });
 
 app.listen(port);
