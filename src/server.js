94fe5c466de2d9d99ad5e47e05926829e364e4cb
Elliot Brooks <elliot.brooks@sky.uk>
2024-02-02 12:27:40 +0000

    added configuration for node app, added package.json added server.js, ignore index.js for now

---
+++ src/server.js
@@ -0,0 +1,13 @@
+
+import express from 'express';
+import path from 'path'
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
