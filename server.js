5d2724e8c395b0dd25165cac8645d623e7ea26d3
Elliot Brooks <elliot.brooks@sky.uk>
2024-02-05 15:36:53 +0000

    added basic config for linting in project

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
+import express from 'express'
+import { initHttpServer } from './src/script/httpServer.js'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-import express from 'express';
-import { initHttpServer } from './src/script/httpServer.js';
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
-
-let app = express();
-const port = process.env.PORT || 8080;
+let app = express()
+const port = process.env.PORT || 8080
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename);
+const __dirname = dirname(__filename)
 
-app.use(express.static('public'));
-app = initHttpServer(port, app, __dirname);
-
-
-
-
-
-
+app.use(express.static('public'))
+app = initHttpServer(port, app, __dirname)
--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 const express = require('express')
-const { spawn } = require('child_process');
+const { spawn } = require('child_process')
 const app = express()
 const port = 3000
 
-
 app.get('/', (req, res) => {
-    const python = spawn('python', ['script.py']);
+    const python = spawn('python', ['script.py'])
     // collect data from script
     python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
+        console.log('Pipe data from python script ...')
+        dataToSend = data.toString()
+    })
     // in close event we are sure that stream from child process is closed
     python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
+        console.log(`child process close all stdio with code ${code}`)
         // send data to browser
         res.send(dataToSend)
-    });
-
+    })
 })
 
 app.listen(port, () => {
