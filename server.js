ff92e320750b93a0b1b9be18dcde7485e4741ac9
Elliot Brooks <elliot.brooks@sky.uk>
2024-02-05 16:13:08 +0000

    added husky linting module

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { initHttpServer } from './src/script/httpServer.js'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import { setDefaultConfig } from 'eslint/lib/rule-tester/rule-tester.js'
 
 let app = express()
 const port = process.env.PORT || 8080
@@ -10,9 +11,3 @@ const __dirname = dirname(__filename)
 
 app.use(express.static('public'))
 app = initHttpServer(port, app, __dirname)
-kjdbjdsd;;;cachesdsd
-dispatchEventc
-zs
-satisfiesc
-s
-ass 
