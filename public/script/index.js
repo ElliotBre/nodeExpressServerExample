d3a657eac1c5dc306e3a8a55a7e6431cd1265446
Elliot Brooks <elliot.brooks@sky.uk>
2024-02-12 12:02:24 +0000

    test

--- public/script/index.js
+++ public/script/index.js
@@ -1,6 +1,6 @@
-console.log('Client-side code running')
+console.log('Client-side code running');
 
-const button = document.getElementById('test')
-button.addEventListener('click', function (e) {
-    console.log('button was clicked')
-})
+const button = document.getElementById('test');
+button.addEventListener('click', (e) => {
+  console.log('button was clicked');
+});
