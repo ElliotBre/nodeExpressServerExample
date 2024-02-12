1e7575df3c117c8a1e530d23b2b5f432dd123e26
Elliot Brooks <elliot.brooks@sky.uk>
2024-02-12 12:16:23 +0000

    test

--- server/script/httpServer.js
+++ server/script/httpServer.js
@@ -5,7 +5,7 @@ export default (port, app, __dirname) => {
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
   });
 
-           app.listen(port);
+       app.listen(port);
   console.log(`Server started at http://localhost:${port}`);
 
   return app;
